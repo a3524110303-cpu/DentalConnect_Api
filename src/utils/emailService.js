67a4a400b9e62c2e6305f4419a335283deffb607
a352410303-cpu <a352410303@alumno.uttehuacan.edu.mx>
2026-03-01 18:12:44 -0600

    Fix emailService

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -10,6 +10,9 @@ const enviarCorreoRecuperacion = async (email, resetUrl) => {
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
+        },
+        tls: {
+            rejectUnauthorized: false
         }
     });
 
